fd1fc9a5a9a699de084f7343cde0e34acfaf5a14
bberak <bberak@gmail.com>
2021-07-05 01:27:57 +1000

    Fixed truthines issues with equality/inequality comparisons

--- src/interpreter.js
+++ src/interpreter.js
@@ -3,7 +3,15 @@ const TokenType = require("./token-type");
 function Interpreter(expression) {
 	const evaluate = (expression) => expression.accept(this);
 
-	this.interpret = () => expression.accept(this);
+	const isTruthy = (value) => {
+		//-- Lox has stricter definitions of truthiness than JavaScript
+		if (value === null) return false;
+		if (typeof value === "boolean") return value;
+
+		return true;
+	};
+
+	this.interpret = () => evaluate(expression);
 	
 	this.visitBinary = (binary) => {
 		switch (binary.operator.type) {
@@ -11,8 +19,8 @@ function Interpreter(expression) {
 			case TokenType.PLUS: return evaluate(binary.left) + evaluate(binary.right);
 			case TokenType.SLASH: return evaluate(binary.left) / evaluate(binary.right);
 			case TokenType.STAR: return evaluate(binary.left) * evaluate(binary.right);
-			case TokenType.BANG_EQUAL: return evaluate(binary.left) !== evaluate(binary.right);
-			case TokenType.EQUAL_EQUAL: return evaluate(binary.left) === evaluate(binary.right);
+			case TokenType.BANG_EQUAL: return isTruthy(evaluate(binary.left)) !== isTruthy(evaluate(binary.right));
+			case TokenType.EQUAL_EQUAL: return isTruthy(evaluate(binary.left)) === isTruthy(evaluate(binary.right));
 			case TokenType.GREATER: return evaluate(binary.left) > evaluate(binary.right);
 			case TokenType.GREATER_EQUAL: return evaluate(binary.left) >= evaluate(binary.right);
 			case TokenType.LESS: return evaluate(binary.left) < evaluate(binary.right);
@@ -29,7 +37,7 @@ function Interpreter(expression) {
 	this.visitUnary = (unary) => {
 		switch (unary.operator.type) {
 			case TokenType.MINUS: return -evaluate(unary.right); 
-			case TokenType.BANG: return !evaluate(unary.right);
+			case TokenType.BANG: return !isTruthy(evaluate(unary.right));
 			
 			default: throw new Error(`Cannot handle ${unary.operator.type} operator`);
 		}
